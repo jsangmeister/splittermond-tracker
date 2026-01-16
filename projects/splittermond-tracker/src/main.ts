ea408f9ecedd4a6e08c4c32b38b6fd9e1829aa8e
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2026-01-16 23:11:13 +0100

    Go zoneless

--- projects/splittermond-tracker/src/main.ts
+++ projects/splittermond-tracker/src/main.ts
@@ -1,10 +1,10 @@
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection()],
+  providers: [provideZonelessChangeDetection()],
 }).catch((err: unknown) => {
   console.error(err);
 });
