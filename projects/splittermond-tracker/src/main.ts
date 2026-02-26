bdb58a563cc30049b974d1f50976903066c24cfb
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2026-02-26 20:08:14 +0100

    Add markdown styling

--- projects/splittermond-tracker/src/main.ts
+++ projects/splittermond-tracker/src/main.ts
@@ -2,12 +2,17 @@ import { registerLocaleData } from '@angular/common';
 import de from '@angular/common/locales/de';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { marked } from 'marked';
 import { de_DE, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { provideMarkdown } from 'ngx-markdown';
 
 import { AppComponent } from './app/app.component';
 
 registerLocaleData(de);
+
+marked.use({
+  gfm: true,
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
